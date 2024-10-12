e69ec4b4b2b039f72272b2f99122724d0f64acc5
vidhiagrawa <vidhiagrawal2205@gmail.com>
2024-10-12 08:39:37 -0700

    added files

--- easy/string/reverseString.java
+++ easy/string/reverseString.java
@@ -1,21 +1,23 @@
 public class reverseString {
     public String r(String s, int k) {
-        // StringBuilder sb = new StringBuilder(s);
+        StringBuilder sb = new StringBuilder(s);
         if( s.length() < k ){
             return new StringBuilder(s).reverse().toString();
             // System.out.println(s);
         }
-        else if( s.length() < 2 * k ){
-            return new StringBuilder(s.substring(0, k)).reverse().toString() + s.substring(k);
-        }
+        // else if( s.length() < 2 * k ){
+        //     return new StringBuilder(s.substring(0, k)).reverse().toString() + s.substring(k);
+        // }
         else {
             return new StringBuilder(s.substring(0, k)).reverse().toString() +
                    s.substring(k, 2 * k) + r(s.substring(2 * k), k);
         }
     }
     public static void main(String[] args) {
-        String s = "abdsjk";
-        int k = 2;
+        reverseString obj = new reverseString();
+        obj.r("abdsjk", 2);
+        // String s = "abdsjk";
+        // int k = 2;
         
     }
 }
