632b7f47d8b889af7e4b9bb4a2d238c0e5213901
vidhiagrawa <vidhiagrawal2205@gmail.com>
2024-10-13 03:05:00 -0700

    adding something new

---
+++ Arrays/easy/kadanesAlgo.java
@@ -0,0 +1,20 @@
+public class kadanesAlgo {
+    public int kda() {
+        int arr[] = { 2, 4, 87, 3, 9 };
+        int curr = 0;
+        int maxS = Integer.MIN_VALUE;
+        for (int each : arr) {
+            curr += each;
+            maxS = Math.max(curr, maxS);
+            if( curr < 0 ){
+                curr = 0;
+            }
+        }
+        System.out.println(maxS);
+        return maxS;
+    }
+    public static void main(String[] args) {
+        kadanesAlgo obj = new kadanesAlgo();
+        obj.kda();
+    }
+}
