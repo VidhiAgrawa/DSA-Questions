5e2003d21a807c19457c67f589b50b5d79b7593f
vidhiagrawa <vidhiagrawal2205@gmail.com>
2024-10-19 12:47:09 -0700

    added

---
+++ Arrays/Intermediate/PairSum.java
@@ -0,0 +1,55 @@
+import java.util.Vector;
+
+public class PairSum {
+    public static void main(String[] args) {
+        // Vector<Integer> arr = new Vector<>();
+        // arr.add(2);
+        // arr.add(7);
+        // arr.add(11);
+        // arr.add(15);
+        int arr[] = { 2, 7, 11, 15 };
+        int target = 26;
+        PairSum(arr, target);
+    }
+
+    public static void PairSum( int arr[], int target ) {
+        int i = 0, j = arr.length - 1;
+        while( i < j ){
+            int PairSum = arr[i] + arr[j];
+            if ( PairSum == target ){
+                System.out.println("found :- " + arr[i] + "," + arr[j]);
+                return;
+            }
+            else if( PairSum < target ){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+    }
+    // public static void PairSum ( Vector<Integer> arr, int target ){
+
+    //     int i = 0;
+    //     int j = arr.size() - 1;
+    //     Vector<Integer> answer;
+
+    //     while ( i < j ) {
+    //         int PairSum = arr.get(i) + arr.get(j);
+            
+    //         if ( PairSum == target ){
+    //             System.out.println("found :- " + arr.get(i) + "," + arr.get(j));
+    //             return;
+    //         }
+    //         else if( PairSum < target ){
+    //             i++;
+    //         }
+    //         else{
+    //             j++;
+    //         }
+    //     }
+
+    // }
+        
+}
+
--- Strings/Easy/longest_com_prefix.java
+++ Strings/Easy/longest_com_prefix.java
@@ -8,45 +8,45 @@ public class longest_com_prefix {
     public static String longest_Common_Prefix(String[] strs) {
 
         if( strs.length == 0 ) return "";
-
-        // finding minimum length of string
+        // finding minimum length of String
         int minLength = Integer.MAX_VALUE;
         for (String str : strs) {
-            minLength = Math.min(minLength, str.length());
+            minLength = Math.min(minLength, str.length());    
         }
-
-        // now searching for common prefix
-
-        String result = "";
+        // now searching for common preffix
+        StringBuilder prefix = new StringBuilder();
         for (int i = 0; i < minLength; i++) {
-            // if( strs[i].isEmpty() ) return "";
-            char word = strs[0].charAt(i);
-            for (String string : strs) {
-                if( string.charAt(i) != word ){
-                    return result;    
-                }
+                char word = strs[0].charAt(i);
+            for (int j = 0; j < strs.length; j++) {
+                if( strs[j].charAt(i) != word ){
+                        return prefix.toString();        
+                }   
             }
-            result += word;
+            prefix.append(word);    
         }
-        return result;
-    }
+        return prefix.toString();
+    }   
 
-    //     // finding minimum length of String
-    //     int minLength = Integer.MAX_VALUE;
-    //     for (String str : strs) {
-    //         minLength = Math.min(minLength, str.length());    
-    //     }
-    //     // now searching for common preffix
-    //     StringBuilder prefix = new StringBuilder();
-    //     for (int i = 0; i < minLength; i++) {
-    //         char word = strs[0].charAt(i);
-    //         for (int j = 0; j < strs.length; j++) {
-    //             if( strs[j].charAt(i) != word ){
-    //                 return prefix.toString();        
-    //             }   
-    //         }
-    //         prefix.append(word);    
-    //     }
-    //     return prefix.toString();
-    // }   
+    
 }
+
+    // // finding minimum length of string
+    // int minLength = Integer.MAX_VALUE;
+    // for (String str : strs) {
+    //     minLength = Math.min(minLength, str.length());
+    // }
+
+    // // now searching for common prefix
+
+    // String result = "";
+    // for (int i = 0; i < minLength; i++) {
+    //     // if( strs[i].isEmpty() ) return "";
+    //     char word = strs[0].charAt(i);
+    //     for (String string : strs) {
+    //         if( string.charAt(i) != word ){
+    //             return result;    
+    //         }
+    //     }
+    //     result += word;
+    // }
+    // return result;
