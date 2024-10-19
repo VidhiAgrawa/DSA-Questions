9b53f236c1448767cddcf4a1aa931d35899c5f8f
vidhiagrawa <vidhiagrawal2205@gmail.com>
2024-10-18 21:53:40 -0700

    adding some new

---
+++ Strings/Easy/longest_Common_Prefix.java
@@ -0,0 +1,27 @@
+public class longest_Common_Prefix {
+    public static void main(String[] args) {
+        String[] strs = { "flower", "flow", "flight" };
+        String st = longest_Common_Prefix(strs);
+        System.out.println(st);
+    }
+    
+    public static String longest_Common_Prefix(String[] strs) {
+        // finding minimum length of String
+        int minLength = Integer.MAX_VALUE;
+        for (String str : strs) {
+            minLength = Math.min(minLength, str.length());    
+        }
+        // now searching for common preffix
+        StringBuilder prefix = new StringBuilder();
+        for (int i = 0; i < minLength; i++) {
+            char word = strs[0].charAt(i);
+            for (int j = 0; j < strs.length; j++) {
+                if( strs[j].charAt(i) != word ){
+                    return prefix.toString();        
+                }   
+            }
+            prefix.append(word);    
+        }
+        return prefix.toString();        
+    }
+}
