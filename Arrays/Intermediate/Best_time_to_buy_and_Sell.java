595ccd0dd8e5fbdedceeea0a93795d1f9be15a21
vidhiagrawa <vidhiagrawal2205@gmail.com>
2024-10-19 20:19:09 -0700

    adding new algo

--- Arrays/Intermediate/Best_time_to_buy_and_Sell.java
+++ Arrays/Intermediate/Best_time_to_buy_and_Sell.java
@@ -1,32 +1,23 @@
 public class Best_time_to_buy_and_Sell {
     public static void main(String[] args) {
         int arr[] = { 7, 3, 5, 9, 6, 1 };
-        int min = minimum(arr);
-        // System.out.println(min);
-        int maxPro = maxProfit(arr, min);
+        int maxPro = maxProfit(arr);
         System.out.println(maxPro);
         
     }
-    public static int minimum(int arr[]) {
-        int min = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if( arr[i] < arr[min] ){
-                min = i;
-                System.out.println(min);
-            }
-        }
-        return min;
-    }
-    public static int maxProfit(int arr[], int min) {
+    
+    public static int maxProfit(int arr[]) {
         if( arr == null || arr.length == 0 ) return 0;
-        int max = 0;
-        for (int i = min+1; i < arr.length; i++) {
-            int profit = arr[i] - arr[min];
-            if( profit > max ){
-                max = profit;
+        int min = arr[0]; // 7 -> 3 -> 1
+        int maxProfit = 0; // 2 -> 6
+        for (int i = 0; i < arr.length; i++) {
+            if( arr[i] < min ){
+                min = arr[i];
             }
-        
+            else if( arr[i] - min > maxProfit ){ //5-3=2 -> 9-3=6 -> 6-3=3
+                maxProfit = arr[i] - min;// 2
+            }
         }
-        return max;
+        return maxProfit;
     }
 }
