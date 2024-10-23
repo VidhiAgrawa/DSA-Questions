2979b3afa0c14b609e979c7e24b0efc41340882e
vidhiagrawa <vidhiagrawal2205@gmail.com>
2024-10-23 04:09:08 -0700

    ADDED NEW

---
+++ Sorting/Learning/selection_sort.java
@@ -0,0 +1,22 @@
+public class selection_sort {
+    static void sort(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if( arr[j] < arr[minIndex] ){
+                    int temp = arr[j]; 
+                    arr[j] = arr[minIndex];
+                    arr[minIndex] = temp;
+                }
+            }
+            
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = { 12, 10, 43, 76, 9, 5 };
+        sort(arr);
+    }
+}
