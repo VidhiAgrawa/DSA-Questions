47b30546343f1dccb0b582d0fba220a9303de541
vidhiagrawa <vidhiagrawal2205@gmail.com>
2024-10-22 18:43:43 -0700

    adding

---
+++ Sorting/Learning/bubble_sort.java
@@ -0,0 +1,21 @@
+public class bubble_sort {
+    static void bubble_sort(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if( arr[i] > arr[j] ){
+                    int n = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = n;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = { 12, 10, 43, 76, 9, 5 };
+        bubble_sort(arr);
+
+    }
+}
