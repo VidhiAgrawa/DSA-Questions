9da8277bf332831ac84ae522bc72a4e37483a682
vidhiagrawa <vidhiagrawal2205@gmail.com>
2024-10-26 06:11:16 -0700

    new Queue

--- Queue/learning/Creation.java
+++ Queue/learning/Creation.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
 
 public class Creation {
+    // for creating queue 
     static void create(int queue[]){
+        // there will be front and rare always
         int front , rare;
+        // inisialized from 0
         front = rare = 0;
+        // loop for asking element in queue from user
         for (int i = 0; i < queue.length; i++) {
             System.out.println("Enter the element in Queue " + i + " :- ");
             Scanner sc = new Scanner(System.in);
             int element = sc.nextInt();
+            // giving the value from user to array of queue
             queue[i] = element;
+            // and increasing value of rare
             rare++;
         }
+        // only printing
         System.out.println("Before Deletion:- ");
         for (int i = front; i < rare; i++) {
             System.out.print(queue[i] + "\t");
             
         }
+        // increaing front to delete the first element
         front++;
         System.out.println("\nAfter Deletion:- ");
         for (int i = front; i < rare; i++) {
--- Queue/learning/NewQueue.java
+++ Queue/learning/NewQueue.java
@@ -1,29 +1,46 @@
 import java.util.*;
+// creating queue 
 public class NewQueue {
+    // creating two variables rear and front where rare is always initialized with -1 
     int rear = -1;
     int front = 0;
     static{
+        // asking for the length of array
         System.out.print("Enter length of Queue:- ");
     }
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
+    //  array will gonna run till that length
     int arr[] = new int[n];
 
     void Insert( int value){
-
-        rear++;
-        arr[rear] = value;
-        System.out.print(arr[rear] + "\t");
-
-        
-    }
-    int remove(){
-        System.out.println("After:- ");
-        front++;
-        for (int i = 0; i < rear; i++) {
-            arr[i] = arr[i+1];
-            System.out.print(arr[i] + "\t");
+        if( rear < n - 1 ){
+            //inserting element int the queue 
+            // increasing rare from -1 to 0
+            rear++;
+            // using rare as index for array and giving value in it 
+            arr[rear] = value;
+            System.out.print(arr[rear] + "\t"); 
         }
+        else{
+            System.out.println("\nQueue is full cannot add more element to it");
+        }
+    }
+    // we will create a remove function
+    int remove(){
+        if( front > rear ){
+            System.out.println("Queue is empty");
+        }
+        else{
+            System.out.println("\nAfter:- ");
+            // the loop runs from front  = 0 to rear = n 
+            for (int i = front; i < rear; i++) {
+                //now shifting value of 1st index at 0th index
+                arr[i] = arr[i+1];
+                System.out.print(arr[i] + "\t");
+            }
+        }
+        // after shifting the last index will gonna be empty so dcrementing the rear from n-1 to n-2
         rear--;
         return front;
     }
@@ -33,6 +50,7 @@ public class NewQueue {
         obj.Insert(10);
         obj.Insert(20);
         obj.Insert(30);
+        obj.Insert(40);
         obj.remove();
     }
     
