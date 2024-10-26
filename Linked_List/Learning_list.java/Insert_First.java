abcd50558ffc0db5bf47c37844b49ae538bc848a
vidhiagrawa <vidhiagrawal2205@gmail.com>
2024-10-25 22:30:20 -0700

    inserting element at first in linked list

---
+++ Linked_List/Learning_list.java/Insert_First.java
@@ -0,0 +1,51 @@
+class Node{
+    int data;
+    Node next;
+    Node( int val ){
+        this.data = val;
+        this.next = null;
+    }     
+}
+class LinkedList{
+    Node head;
+    LinkedList(){
+        head = null;
+    }
+    void insertFirst( int data ){
+        Node newNode1 = new Node(data);
+        newNode1.next = head;
+        head = newNode1;
+    }
+    void insert( int data ){
+        Node newNode = new Node(data);
+        if ( head == null ){
+            head = newNode;
+        }
+        else{
+            Node temp = head;
+            while ( temp.next != null ) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+    void display(){
+        Node temp = head;
+        while ( temp != null ) {
+            System.out.print( temp.data + " -> " );
+            temp = temp.next;
+        }
+        System.out.print("null");
+    }
+}
+public class Insert_First {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insertFirst(0);
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.display();
+        
+    }
+}
