ee08ef55b4eff3640e41ff24e3ee3c2de24ed011
vidhiagrawa <vidhiagrawal2205@gmail.com>
2024-11-28 21:12:09 -0800

    adding

--- Tree/BSTTraversals.java
+++ Tree/BSTTraversals.java
@@ -58,6 +58,32 @@ class Tree{
             System.out.print(root.data + " ");
         }
     }
+    void inorder() {
+        System.out.print("In-order Traversal: ");
+        inorderRec(root);
+        System.out.println();
+    }
+        
+    private void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data + " ");
+            inorderRec(root.right);
+        }
+    }
+    void preorder() {
+        System.out.print("Pre-order Traversal: ");
+        preorderRec(root);
+        System.out.println();
+    }
+    
+    private void preorderRec(Node root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preorderRec(root.left);
+            preorderRec(root.right);
+        }
+    }
 } 
 
 public class BSTTraversals  {
@@ -80,32 +106,5 @@ public class BSTTraversals  {
 }
 
 
-// void inorder() {
-    //     System.out.print("In-order Traversal: ");
-    //     inorderRec(root);
-    //     System.out.println();
-    // }
-    
-    // private void inorderRec(Node root) {
-        //     if (root != null) {
-            //         inorderRec(root.left);
-            //         System.out.print(root.data + " ");
-        //         inorderRec(root.right);
-        //     }
-        // }
 
 
-
-        // void preorder() {
-        //     System.out.print("Pre-order Traversal: ");
-        //     preorderRec(root);
-        //     System.out.println();
-        // }
-        
-        // private void preorderRec(Node root) {
-        //     if (root != null) {
-        //         System.out.print(root.data + " ");
-        //         preorderRec(root.left);
-        //         preorderRec(root.right);
-        //     }
-        // }
