d06e13f154ddba0318c6ce0410c61dab419bef45
Vidhi Agrawal <163636860+vidhiagrawa@users.noreply.github.com>
2024-12-07 15:23:25 +0000

    adding

--- Arrays/easy/swap.c
+++ Arrays/easy/swap.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
+// #include<stdio.h>
 
 // void swapping(int *a, int *b);
 
-int main(){
+// int main(){
     // int x = 10, y = 20;
     // swapping(&x, &y);
     // printf(" value of a = " "%d\n", x);
     // printf( " value of b = " "%d\n", y);
-    int a = 5,b = 10;
-    a += a <= b;
-    printf("output :- %d" , a);
-    return 0;
-}
+//     int a = 5,b = 10;
+//     a += a <= b;
+//     printf("output :- %d" , a);
+//     return 0;
+// }
 
 
 // void swapping(int *a, int *b){
